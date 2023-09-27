6be1035c1f6d4bb9d32a41f46c6fa61dd060ad4f
Samuel Vítek <samuel.vitek@email.cz>
2023-09-27 13:43:08 +0300

    Finalizing the project, documenting code, readme creation styling the results

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
-# case_study_dice_game
-Case study - Empire state building dice game
-# TODO readme
+# Case study - dice game probability
+This case study analyze the probability of winning a die game.
+
+There were NumPy random numbers used with the seed 123.
+All the results are based on this seeds pseudo random numbers.
+
+### Rules of the game
+A person begins with 0 points and his goal is to be able to 60 points within 100 die throws.\
+Rules fo each side of the dice:
+* **1, 2 -** user **loses one point** from his overall score (cannot go lower than 0)
+* **3, 4, 5 -** user **gains one point** to his overall score
+* **6 -** user gains as many points as his next throws number worth (if next throws number is 4 he gains 4 points)
+
+There is **0.1% chance** in each throw to _"loose all points"_ and return back to overall score 0
+
+### Case study
+In this case study the game **is played  500 times.**\
+Result of this case study is histogram of the most repeated final overall scores (Figure 1).
+Second result of the case study is probability of succeeding in the game in percentage.
+
+### Results of case study
+![histogram.png](histogram.png)\
+Figure 1: Final points and their occurrences histogram.
--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# TODO readme
-# Set initial values
+# Set initial values (seed for random, empty list for all walks)
 np.random.seed(123)
-step = 50
-dice = np.random.randint(1, 7)
 all_walks = []
 
+# Trying to play 500 times
 for x in range(500):
     random_walk = [0]
 
+    # 100 die throws
     for y in range(100):
         step = random_walk[-1]
         dice = np.random.randint(1, 7)
 
+        # Point addition decision-making
         if dice <= 2:
             step = max(0, step - 1)
         elif dice <= 5:
@@ -22,28 +22,26 @@ for x in range(500):
         else:
             step = step + np.random.randint(1, 7)
 
+        # Returning to 0 check
         if np.random.rand() <= 0.001:
             step = 0
 
         random_walk.append(step)
     all_walks.append(random_walk)
 
+# Preparing data (transpose - switch rows with cols, selecting only last points)
 np_aw = np.transpose(np.array(all_walks))
 finishes = np_aw[-1, :]
 
-ranges = [x * 10 for x in range(0, 12)]
+# Probability calculation
+possibility = round((np.count_nonzero(finishes >= 60) / 500) * 100, 2)
 
-# TODO style the plot
-# plt.hist(finishes, bins=10, edgecolor='black', align='left')
-# plt.xticks(ranges)
-# #plt.hist(finishes)
-# plt.show()
+# Plotting the
+font = {'size': 11}
 
-plt.hist(finishes)
+plt.hist(finishes, bins=10, edgecolor='black')
+plt.title('Final points and their occurrences', loc='left', fontdict=font)
+plt.title('Probability of win: ' + str(possibility) + '%', loc='right', fontdict=font)
+plt.xlabel("Count of final points")
+plt.ylabel("Occurrences")
 plt.show()
-
-
-# TODO style the response
-succ_fin = finishes[finishes > 60]
-possibility = (np.count_nonzero(finishes >= 60) / 500) * 100
-print(str(possibility) + '%')
